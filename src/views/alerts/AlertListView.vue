03db3232ce324890bfafd2c1c25d238435217376
w-taek <71427194+w-taek@users.noreply.github.com>
2026-04-14 17:53:08 +0900

    Revert "Feat/fe2 layout router v2 - [FE] 장비 상세 페이지 경로 및 기본 화면 추가"

--- src/views/alerts/AlertListView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>알림 이력 페이지</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
--- src/views/maintenance/MaintenanceListView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>정비 페이지</h1>
-  </div>
-</template>
-
-<script setup>
-</script>
